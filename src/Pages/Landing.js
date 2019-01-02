c4cb92bd6b33f9e305ff0f42012cbfe649f1b3c1
Ariel Chen <arielchen232@Ariels-MacBook-Pro.local>
2019-01-02 13:36:49 -0600

    no more blahs

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -7,6 +7,8 @@ import './Landing.css'
 import imageName from "../Assets/just_logo.png"
 
 import {
+    CardBody,
+    Card,
     Collapse,
     Navbar,
     NavbarToggler,
@@ -21,49 +23,64 @@ import {
     Button
 } from 'reactstrap';
 
-class Landing extends Component {
+class Landing extends React.Component {
 
   constructor(props) {
-          super(props);
+    super(props);
 
-          this.toggleNavbar = this.toggleNavbar.bind(this);
-     this.state = {
-       collapsed: false
-     };
-   }
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.toggle2 = this.toggle2.bind(this);
+    this.toggle3 = this.toggle3.bind(this);
+    this.state = {
+      collapsed: true,
+      collapse: false,
+      collapse2: false,
+      collapse3: false,
+    };
+  }
 
-    toggleNavbar() {
-     this.setState({
-       collapsed: !this.state.collapsed
-     });
-   }
+  toggleNavbar() {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
 
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
 
+  toggle2() {
+    this.setState({ collapse2: !this.state.collapse2 });
+  }
+
+  toggle3() {
+    this.setState({ collapse3: !this.state.collapse3 });
+  }
 
   render() {
     return (
       <div className="landing">
 
-      <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top" id="navHome">
-          <a class="navbar-brand js-scroll-trigger" href="#">Home</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="#navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
-              <li class="nav-item active">
-                <a class="nav-link js-scroll-trigger" href="#about">About</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link js-scroll-trigger" href="#team">Team</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link js-scroll-trigger" href="#doc">Project Docs</a>
-              </li>
-            </ul>
-          </div>
-      </nav>
+      <div>
+        <Navbar color="muted" light expand="lg">
+          <NavbarBrand href="/" className="mr-auto">Home</NavbarBrand>
+          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="#about">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#team">Team</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#doc">Project Docs</NavLink>
+            </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
 
       <div className="landing_top">
         <div class="container">
@@ -80,25 +97,46 @@ class Landing extends Component {
 
       <div className="landing_bottom">
         <section id="about">
-        <div className="Jumbotron">
-          <h2> About whatsRoaring </h2>
-          <p> blah blah text </p>
-        </div>
+        <Button color="muted" size="lg" block onClick={this.toggle} style={{ marginBottom: '1rem' }}>About whatsRoaring</Button>
+        <Collapse isOpen={this.state.collapse}>
+          <Card>
+            <CardBody>
+            Currently, each group on campus that has events either has a separate calendar or list of events on its website or publicize its events only through listserv blasts, posters, facebook, or word of mouth. Having event information in so many different places makes it very difficult to keep track of events you want to attend, and makes it very easy to not even learn about events until after they have already happened. Our system provides a platform on which club presidents and other event planners can post events and other users can find events they are interested in.
+
+            There is currently a university-run event website, https://www.princeton.edu/events, but it does not allow for favoriting or saving events, and it does not show events in calendar format. It also only includes official university events, and not events put on by clubs. Our calendar would have all of these functionalities. Google also provides a list of events if you google “princeton events,” but once again there is no way to favorite or save events, the interface is very difficult to read, especially since the events are not listed in chronological order, and it does not allow for any sort of filtering.
+            </CardBody>
+          </Card>
+        </Collapse>
         </section>
 
         <section id="team">
-        <div className="Jumbotron">
-          <h2> The Team </h2>
-          <p> blah blah pics and text </p>
-        </div>
+        <Button color="muted" size="lg" block onClick={this.toggle2} style={{ marginBottom: '1rem' }}>The Team</Button>
+        <Collapse isOpen={this.state.collapse2}>
+          <Card>
+            <CardBody>
+            [section coming soon!]
+            </CardBody>
+          </Card>
+        </Collapse>
         </section>
 
         <section id="doc">
-        <div className="Jumbotron">
-        <h2> Project Documentation </h2>
-        <p> blah blah stuff, links prolly </p>
-        </div>
+        <Button color="muted" size="lg" block onClick={this.toggle3} style={{ marginBottom: '1rem' }}>Project Documentation</Button>
+        <Collapse isOpen={this.state.collapse3}>
+          <Card>
+            <CardBody>
+            [section coming soon!]
+            </CardBody>
+          </Card>
+        </Collapse>
         </section>
+        </div>
+
+        <div className="footer">
+          <div class="container">
+            <span class="text-muted">Thanks for using whatsRoaring!
+            </span>
+          </div>
         </div>
 
 
