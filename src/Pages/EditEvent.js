2c986f1d42c59a8baa6b78cc8a6687a857a6d6cf
Rebecca <rb25@princeton.edu>
2019-01-13 00:06:39 +0200

    edit event tz

--- src/Pages/EditEvent.js
+++ src/Pages/EditEvent.js
@@ -188,7 +188,7 @@ class EditEvent extends Component {
       this.setState({ startTime: '' })
     } else {
       var startDate = new Date(this.props.start)
-      startDate.setHours(startDate.getHours() - 5)
+      startDate.setTime(startDate.getTime() - startDate.getTimezoneOffset()*60*1000);
       var startStr = startDate.toISOString()
       this.setState({ startTime: startStr.substring(0, startStr.length-1) })
     }
@@ -197,7 +197,7 @@ class EditEvent extends Component {
       this.setState({ endTime: '' })
     } else {
       var endDate = new Date(this.props.end)
-      endDate.setHours(endDate.getHours() - 5)
+      endDate.setTime(endDate.getTime() - endDate.getTimezoneOffset()*60*1000);
       var endStr = endDate.toISOString()
       this.setState({ endTime: endStr.substring(0, endStr.length-1) })
     }
