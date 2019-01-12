df96dfef1c14282a07ab4b66a1d87f0d957096c4
Berthy Feng <bfeng@princeton.edu>
2019-01-12 16:38:48 -0500

    Remove text transform from buttons

--- src/Pages/Components/LogOutButton.js
+++ src/Pages/Components/LogOutButton.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import { MuiThemeProvider } from '@material-ui/core/styles'
+import { withStyles } from '@material-ui/core/styles'
 import Theme from '../../Assets/Theme'
 import Button from '@material-ui/core/Button'
 
 import './Button.css'
+
+const styles = theme => ({
+  button: {
+    width: 100,
+    margin: 10,
+    textTransform: 'none',
+  }
+})
 
 class LogOutButton extends Component {
   constructor() {
@@ -18,13 +27,19 @@ class LogOutButton extends Component {
   }
 
   render () {
+    const { classes } = this.props
     return (
       <MuiThemeProvider theme={Theme}>
-        <Button variant="outlined" color="primary" onClick={this.handleClick} size="medium">
+        <Button 
+          variant="outlined" 
+          color="primary" 
+          onClick={this.handleClick} 
+          size="medium"
+          className={classes.button}>
             Log Out
         </Button>
       </MuiThemeProvider>
     )
   }
 }
-export default withRouter(LogOutButton);
+export default withStyles(styles)(withRouter(LogOutButton));
